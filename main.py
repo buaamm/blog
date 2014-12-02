611717500fa52df2ebaf9f34ef786339dea47509
buaamm <buaamm@gmail.com>
2014-12-02 20:52:23 +0800

    Add Tables, Implemented Resgister Function

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ app_path = os.getcwd()
 DATABASE = '/tmp/blog.db'
 DEBUG = True
 SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD ='default'
+#USERNAME = 'admin'
+#PASSWORD ='default'
 UPLOAD_FOLDER = app_path + '/files'
 
 #database utils
@@ -67,38 +67,95 @@ def close_db(error):
     if db is not None:
         db.close()
 
-@app.route('/')
+@app.route('/') #---------- homepage ----------
 def homepage():
     g.db = connect_db()
-    cur = g.db.execute('select title, text from entries order by id desc')
+    cur = g.db.execute('select title, create_time, text from entries order by id desc')
     entries = cur.fetchall()
     return render_template('homepage.html', entries=entries)
 
-@app.route('/add', methods=['POST'])
+@app.route('/add', methods=['POST']) #---------- add_entry ----------
 def add_entry():
     if not session.get('logged_in'):
-        abort(401)
+        abort(401) # will never happen
     db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
+    cur_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    db.execute('insert into entries (userid, title, create_time, text) values (?, ?, ?, ?)',
+            [0, request.form['title'], cur_time, request.form['text']])
     db.commit()
     flash('New entry was successfully posted.')
     return redirect(url_for('homepage'))
 
-@app.route('/login', methods=['GET', 'POST'])
+def get_userid(username):
+    g.db = connect_db()
+    cur = g.db.execute('select id from users where username="%s"' % username)
+    res = cur.fetchall()
+    if len(res) == 0:
+        return -1
+    else:
+        return res[0][0]
+
+@app.route('/sign_up', methods=['GET', 'POST']) #---------- sign up ----------
+def sign_up():
+    error = None
+    if request.method == 'POST':
+        cur_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        username = request.form['username']
+        password = request.form['password']
+        if myhash.check_text(username, password) is False:
+            error = "Invalid Input, please try again."
+            return render_template('sign_up.html', error=error)
+        g.db = connect_db()
+        cur = g.db.execute('select username from users where username="%s"' % username)
+        if len(cur.fetchall()) != 0:
+            error = "Username already exist, please try again."
+            return render_template('sign_up.html', error=error)
+        password = myhash.calc_sha1(password)
+        db = get_db()
+        db.execute('insert into users (username, password, create_time) values (?, ?, ?)',
+                [username, password, cur_time])
+        db.commit()
+        flash('Welcome New user: %s!' % username)
+        session['logged_in'] = True
+        session['username'] = username
+        session['userid'] = get_userid(username)
+        flash('You are logged in, %s' % username)
+        return redirect(url_for('homepage'))
+    return render_template('sign_up.html', error=error)
+
+def login_check(username, password): #---------- login [check]----------
+    if session.get('logged_in'):
+        abort(401) # TODO: SHOW a message "Please logout first"
+    g.db = connect_db()
+    cur = g.db.execute('select id, username, password from users where username="%s"' % username)
+    users = cur.fetchall()
+    if len(users) == 0:
+        return -2
+    else:
+        assert(len(users) == 1)
+        if (users[0][2] == myhash.calc_sha1(password) ):
+            return users[0][0]
+        else:
+            return -1
+
+@app.route('/login', methods=['GET', 'POST']) #---------- login ----------
 def login():
     error = None
     if request.method ==  'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
+        res = login_check(request.form['username'], request.form['password'])
+        if res == -2:
+            error = 'Username not exist!'
+        elif res == -1:
+            error = 'Wrong password!'
         else:
             session['logged_in'] = True
-            flash('You are logged in')
+            session['userid'] = res
+            session['username'] = request.form['username']
+            flash('You are logged in!')
             return redirect(url_for('homepage'))
     return render_template('login.html', error=error)
 
-@app.route('/logout')
+@app.route('/logout') #---------- logout ----------
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
@@ -107,6 +164,7 @@ def logout():
 #@app.route('/profile/<int:user_id>')
 #def profile():
 #    
+
 @app.route('/video')
 def show_video():
     return render_template('show_video.html')
@@ -115,38 +173,47 @@ def show_video():
 def set_music():
     return render_template('set_music.html')
 
-
-ALLOWED_EXT = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp3', 'ogg', 'mp4', 'bmp', 'ico'])
-def check_filename(filename):
-    return ('.' in filename) and (filename.rsplit('.', 1)[1] in ALLOWED_EXT)
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/upload', methods=['GET', 'POST']) #---------- upload ----------
+def upload():
     if request.method == 'POST':
         file = request.files['file'] # use [files] dictionary on the request object
-        if file and check_filename(file.filename):
+        text = request.form['text']
+        if file and myhash.check_filename(file.filename):
             filename = secure_filename(file.filename) # secure_filename(): Strip filename To ASCII string
+            # TODO: filename DIY, user
+            # TODO: file already exist
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) # save(): save file permanently
-            return redirect(url_for('uploaded_file', filename=filename)) # Uploaded Successfully, Redirect to [uploaded_file]
-    # Upload Failed
+            if not session.get('userid'):
+                session['userid'] = -1
+            cur_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+            db = get_db()
+            db.execute('insert into files (userid, filename, upload_time, text) values (?, ?, ?, ?)',
+                    [session['userid'], filename, cur_time, text])
+        db.commit()
+        flash('File Uploaded!')
+        return redirect(url_for('uploaded', filename=filename)) # Uploaded Successfully, Redirect to [uploaded]
+    # Upload Failed ? or List
+    g.db = connect_db()
+    cur = g.db.execute('select id, userid, filename, upload_time, text from files order by upload_time desc')
+    all = cur.fetchall()
+    # ID, UID, filename, up_time, text
     file_dict = []
-    index = 1
-    for afile in os.listdir(app.config['UPLOAD_FOLDER']):
+    for index in xrange(len(all)):
+        aid   = all[index][0]
+        auid  = all[index][1]
+        afile = all[index][2]
+        atime = all[index][3]
+        atext = all[index][4]
         apath = os.path.join(app.config['UPLOAD_FOLDER'], afile)
-        atime = os.path.getmtime(apath)
-        adate = (datetime.datetime.fromtimestamp(atime)).strftime('%Y-%m-%d %H:%M:%S')
         asize = os.path.getsize(apath)
         amd5  = myhash.get_md5(apath)
         asha1 = myhash.get_sha1(apath)
-        file_dict.append( (adate, index, afile, asize, amd5, asha1) )
-        index = index + 1
-
-    files = sorted(file_dict) 
-    return render_template('upload_file.html', files=files)
+        file_dict.append( (index+1, aid, auid, afile, atime, asize, atext, amd5, asha1) )
+    return render_template('upload.html', files=file_dict)
 
 @app.route('/uploaded/<filename>')
 # send_from_directory(): Show a [file] in server
-def uploaded_file(filename):
+def uploaded(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 ######################################
--- myhash.py
+++ myhash.py
@@ -15,6 +15,34 @@ def get_sha1(filepath):
         ret = obj.hexdigest()
     return ret
 
+def calc_md5(text):
+    obj = hashlib.md5()
+    obj.update(text)
+    ret = obj.hexdigest()
+    return ret
+
+def calc_sha1(text):
+    obj = hashlib.sha1()
+    obj.update(text)
+    ret = obj.hexdigest()
+    return ret
+
+
+username_charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+password_charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&()*+,-.:;<=>?@[]^_{|}~ "
+def check_text(username, password):
+    for it in username:
+        if it not in username_charset:
+            return False
+    for it in password:
+        if it not in password_charset:
+            return False
+    return True
+
+ALLOWED_EXT = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp3', 'ogg', 'mp4', 'bmp', 'ico'])
+def check_filename(filename): #---------- upload [check] ----------
+    return ('.' in filename) and (filename.rsplit('.', 1)[1] in ALLOWED_EXT)
+
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         # >>python myhash.py [filepath]

571496d39411a3e8a549b838e8f55613358c7173
buaamm <buaamm@gmail.com>
2014-12-08 14:06:55 +0800

    Reset

--- main.py
+++ main.py
@@ -70,32 +70,51 @@ def close_db(error):
     if db is not None:
         db.close()
 
-@app.route('/') #---------- homepage ----------
+@app.template_filter('getcover')
+def cover_filter(s):
+    L=s.split('|')
+    return L[0]+L[1]
+
+@app.route('/') #---------- homepage ----------OOOOOO
 def homepage():
     g.db = connect_db()
-    cur = g.db.execute('select entries.id, entries.userid, entries.title, entries.create_time, entries.text, users.username from entries, users where entries.userid=users.id order by entries.create_time desc')
-    entries = cur.fetchall()
+    entries = g.db.execute('select entries.id, entries.userid, entries.mid, entries.title, entries.create_time, entries.abstract, entries.text, entries.mname, entries.mtype, entries.murls, users.username from entries, users where entries.userid=users.id order by entries.create_time desc').fetchall()
     return render_template('homepage.html', entries=entries)
 
-@app.route('/add', methods=['POST']) #---------- add_entry ----------
+@app.route('/add', methods=['GET', 'POST']) #---------- add_entry ----------000000
 def add_entry():
+    error = None
     if not session.get('logged_in'):
-        abort(401) # will never happen
-    db = get_db()
-    cur_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-    if not session.get('userid'): session['userid'] = -1
-    db.execute('insert into entries (userid, title, create_time, text) values (?, ?, ?, ?)',
-            [session['userid'], request.form['title'], cur_time, request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted.')
-    return redirect(url_for('homepage'))
+        error = "Please login first!" # will never happen
+        return redirect(url_for('homepage'), error=error)
+    if request.method == 'POST':
+        db = get_db()
+        cur_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        if not session.get('userid'): session['userid'] = -1
+        # Add Item
+        g.db = connect_db()
+        res = g.db.execute('select id from items where name="%s"' % request.form['mname']).fetchall()
+        if len(res) != 0:
+            error = "Media name already exist, try comment."
+            return render_template('add.html', error=error) #return render_template('sign_up.html', error=error)
+        db.execute('insert into items (name,type,chapters,urls,upload_time,description) values (?,?,?,?,?,?)', 
+            [ request.form['mname'], request.form['mtype'], request.form['ch']+"|"+request.form['chall'], request.form['murls'], cur_time, request.form['minfo'] ]) 
+        db.commit()
+        res = g.db.execute('select id from items where name="%s"' % request.form['mname']).fetchall()
+        assert len(res)==1
+        db.execute('insert into entries (userid, mid, title, create_time, abstract, text, mname, mtype, murls) values (?,?,?,?,?,?,?,?,?)',
+            [session['userid'], res[0][0], request.form['title'], cur_time, request.form['abstract'], request.form['text'], request.form['mname'], request.form['mtype'], request.form['murls']])
+        db.commit()
+        flash('New entry was successfully posted.')
+        return redirect(url_for('homepage'))
+    return render_template('add.html')
 
 def get_userid(username):
     g.db = connect_db()
     res = g.db.execute('select id from users where username="%s"' % username).fetchall()
     return -1 if (len(res) == 0) else res[0][0]
 
-@app.route('/sign_up', methods=['GET', 'POST']) #---------- sign up ----------
+@app.route('/sign_up', methods=['GET', 'POST']) #---------- sign up ----------OOOOOO
 def sign_up():
     error = None
     if request.method == 'POST':
@@ -112,41 +131,32 @@ def sign_up():
             return render_template('homepage.html', signup_error=error) #return render_template('sign_up.html', error=error)
         password = myhash.calc_sha1(password)
         db = get_db()
-        db.execute('insert into users (username, password, create_time, birthday, sex, description) values (?, ?, ?, "", -1, "")',
+        db.execute('insert into users (username, password, create_time, nickname, birthday, sex, description, level, cash) values (?, ?, ?, "", "", -1, "", 0, 0)',
                 [username, password, cur_time])
         db.commit()
-        flash('Welcome New user: %s!' % username)
+        flash('Welcome New user: %s!' % username); flash('You are logged in, %s' % username)
         session['logged_in'] = True
         session['username'] = username
         session['userid'] = get_userid(username)
-        flash('You are logged in, %s' % username)
         return redirect(url_for('homepage'))
     return render_template('homepage.html', signup_error=error) #return render_template('sign_up.html', error=error)
 
 def login_check(username, password): #---------- login [check]----------
-    if session.get('logged_in'):
-        abort(401) # TODO: SHOW a message "Please logout first"
+    if session.get('logged_in'): abort(401) # TODO: SHOW a message "Please logout first"
     g.db = connect_db()
-    cur = g.db.execute('select id, username, password from users where username="%s"' % username)
-    users = cur.fetchall()
-    if len(users) == 0:
-        return -2
-    else:
-        assert(len(users) == 1)
-        if (users[0][2] == myhash.calc_sha1(password) ):
-            return users[0][0]
-        else:
-            return -1
+    users = g.db.execute('select id, username, password from users where username="%s"' % username).fetchall()
+    if len(users) == 0: return -2
+    assert(len(users) == 1)
+    if (users[0][2] == myhash.calc_sha1(password) ): return users[0][0]
+    else: return -1
 
-@app.route('/login', methods=['GET', 'POST']) #---------- login ----------
+@app.route('/login', methods=['GET', 'POST']) #---------- login ----------OOOOOO
 def login():
     error = None
     if request.method ==  'POST':
         res = login_check(request.form['username'], request.form['password'])
-        if res == -2:
-            error = 'Username not exist!'
-        elif res == -1:
-            error = 'Wrong password!'
+        if res == -2: error = 'Username not exist!'
+        elif res == -1: error = 'Wrong password!'
         else:
             session['logged_in'] = True
             session['userid'] = res
@@ -155,40 +165,38 @@ def login():
             return redirect(url_for('homepage'))
     return render_template('homepage.html', login_error=error) 
 
-@app.route('/logout') #---------- logout ----------
+@app.route('/logout') #---------- logout ----------OOOOOO
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('homepage'))
 
-@app.route('/profile', methods=['GET', 'POST'])
+@app.route('/profile', methods=['GET', 'POST']) #---------- profile ----------OOOOOO
 def profile():
     if request.method ==  'POST':
         db = get_db()
         if len(request.form['password']) >=3:
             password = myhash.calc_sha1( request.form['password'] )
-            db.execute('update users set password="%s", birthday="%s", description="%s", sex="%s" where id="%s"' % 
-                (password, request.form['birthday'], request.form['description'], request.form['sex'], str(session["userid"])) )
+            db.execute('update users set password="%s", birthday="%s", description="%s", sex="%s", nickname="%s" where id="%s"' % 
+                (password, request.form['birthday'], request.form['description'], request.form['sex'], request.form['nickname'], str(session["userid"])) )
         else:
-            db.execute('update users set birthday="%s", description="%s", sex="%s" where id="%s"' % 
-                (request.form['birthday'], request.form['description'], request.form['sex'], str(session["userid"])) )
+            db.execute('update users set birthday="%s", description="%s", sex="%s", nickname="%s" where id="%s"' % 
+                (request.form['birthday'], request.form['description'], request.form['sex'], request.form['nickname'], str(session["userid"])) )
         db.commit()
         return redirect(url_for('user', username=session["username"]))
     g.db = connect_db()
-    res = g.db.execute('select id, username, create_time, birthday, sex, description from users where id="%s"' % str(session["userid"]) ).fetchall()
+    res = g.db.execute('select id, username, create_time, nickname, birthday, sex, description from users where id="%s"' % str(session["userid"]) ).fetchall()
     assert len(res) == 1 # TODO: Need test
     return render_template('profile.html', userinfo=res[0])
     
-@app.route('/user/<username>')
+@app.route('/user/<username>') #---------- user ----------OOOOOO
 def user(username):
     error = None
     g.db = connect_db()
-    res = g.db.execute('select id, username, create_time, birthday, sex, description from users where username="%s"' % username).fetchall()
+    res = g.db.execute('select id, username, create_time, nickname, level, cash, birthday, sex, description from users where username="%s"' % username).fetchall()
     userinfo = (list)(res[0])
-    userinfo[4] = myhash.sex_repr(userinfo[4])
-    if len(res) == 0:
-        error = "User not found! Show admin instead."
-        res = g.db.execute('select id, username, create_time, birthday, sex, description from users where username="admin"').fetchall()
+    userinfo[7] = myhash.sex_repr(userinfo[7])
+    if len(res) == 0: error = "User not found!"
     return render_template('user.html', userinfo=userinfo, error=error)
     
 
@@ -271,6 +279,35 @@ def blog(id=None):
 def video(id=None):
     if id is None: return render_template('video.html', type="home")
     return render_template('video.html', type=id[0], id=id[1:])
+
+@app.route('/items')
+@app.route('/items/<id>')
+def items(id=None):
+    if id is None: return render_template('homepage.html')
+    g.db = connect_db()
+    res = g.db.execute('select name,type,chapters,urls,upload_time,description from items where id="%s"' % id).fetchall()
+    if len(res) == 0:
+        error = "Media ID not exist."
+        return render_template('homepage.html', error=error) #return render_template('sign_up.html', error=error)
+    assert len(res)==1; value1 = res[0]
+    tp = value1[1]
+    ch,chall = value1[2].split('|')
+    res = g.db.execute('select userid, title, abstract, text from entries where mid="%s"' % id).fetchall()
+    assert len(res)==1; value2 = res[0]
+    if tp[0] in "01": return render_template('anime.html', type=tp[1], id=value1[3], name=value1[0], ch=ch, upload=value1[4], info=value1[5], uid=value2[0], title=value2[1], text=value2[2]+value2[3])
+    divider = value1[3].split('|')
+    if tp[0] in "23": return render_template('comic.html', type=tp[1], name=value1[0], path=divider[0], cover=divider[1], chapters=value1[2], upload=value1[4], info=value1[5], 
+        uid=value2[0], title=value2[1], text=value2[2]+value2[3], filelist=divider[2:] )
+    return render_template('none.html')
+
+@app.route('/delete/<id>')
+def delete(id):
+    db = get_db()
+    db.execute('delete from items where id=%s' % id)
+    db.execute('delete from entries where id=%s' % id)
+    db.commit()
+    return render_template('homepage.html')
+
 
 ######################################
 
--- schema.sql
+++ schema.sql
@@ -1,10 +1,29 @@
+drop table if exists users;
+create table users (
+    id integer primary key autoincrement,
+    username text not null,
+    password text not null,
+    create_time text not null,
+    nickname text not null,
+    birthday text not null,
+    sex integer not null,
+    level integer not null,
+    cash integer not null,
+    description text not null
+);
+
 drop table if exists entries;
 create table entries (
     id integer primary key autoincrement,
     userid integer not null,
+    mid integer not null,
     title text not null,
     create_time text not null,
-    text text not null
+    abstract text not null,
+    text text not null,
+    mname text not null,
+    mtype text not null,
+    murls text not null
 );
 
 drop table if exists files;
@@ -17,13 +36,27 @@ create table files (
 );
 
 
-drop table if exists users;
-create table users (
+drop table if exists items;
+create table items (
     id integer primary key autoincrement,
-    username text not null,
-    password text not null,
-    create_time text not null,
-    birthday text,
-    sex integer,
-    description text
+    name text not null,
+    type text not null,
+    chapters integer not null,
+    urls text not null,
+    upload_time text not null,
+    description text not null
 );
+
+drop table if exists images;
+create table images (
+    id integer primary key autoincrement,
+    userid integer not null,
+    name text not null,
+    type text not null,
+    count integer not null,
+    urls text not null,
+    upload_time text not null,
+    description text not null
+);
+
+
